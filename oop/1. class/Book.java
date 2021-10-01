72b80441d9ba184c855843e66daf029eed3bc8a9
subtitle1 <87356533+subtitle1@users.noreply.github.com>
2021-10-01 13:33:15 +0900

    Add files via upload

---
+++ oop/1. class/Book.java
@@ -0,0 +1,15 @@
+package example_09_06_class;
+import java.util.Date; // 날짜를 나타낼 때 쓰는 import
+
+public class Book {
+	// 필드 정의하기
+	// Book 객체의 고유한 속성은 책 번호, 제목, 저자, 출판사, 가격, 재고, 출판일
+	int bookNo;
+	String title;
+	String genre;
+	String writer;
+	String publisher;
+	int price;
+	int stock;
+	Date pubDate;
+}
